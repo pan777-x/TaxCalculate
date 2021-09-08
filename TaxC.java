3f53d4436435feab03d711bb706714fb5670aafb
LingYan-ly <83797193+LingYan-ly@users.noreply.github.com>
2021-09-08 11:14:50 +0800

    Add files via upload

---
+++ TaxC.java
@@ -0,0 +1,29 @@
+package com.imooc.util;
+
+public class TaxC {
+	public int Tax_C(int salary,int ins){
+		double beyond = salary - ins - 3500;
+		double tax=0;
+		
+		if (beyond <= 0) {
+            tax = 0;
+        } else if (beyond <= 1500) {
+            tax = beyond * 0.03;
+        } else if (beyond <= 4500) {
+            tax = beyond * 0.1 - 105;
+        } else if (beyond <= 9000) {
+            tax = beyond * 0.2 - 555;
+        } else if (beyond <= 35000) {
+            tax = beyond * 0.25 - 1005;
+        } else if (beyond <= 55000) {
+            tax = beyond * 0.3 - 2755;
+        } else if (beyond <= 80000) {
+            tax = beyond * 0.35 - 5505;
+        } else {
+            tax = beyond * 0.45 - 13505;
+        }
+        System.out.println("您的个人所得税是：" + tax);
+        
+        return (int)tax;
+	}
+}
